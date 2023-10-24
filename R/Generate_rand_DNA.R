115487762f405357dc6a85c639bfa00a54c000e3
NoyTii <noy.tabul@gmail.com>
2023-10-24 11:44:56 +0200

    Function1_Noy

---
+++ R/Generate_rand_DNA.R
@@ -0,0 +1,13 @@
+#' Generate_rand_DNA
+#'
+#' @param DNA_len insert the length of the random generated DNA
+#'
+#' @return The function will return a random DNA code with by the parameter length
+#' @export
+#'
+#' @examples Generate_rand_DNA(3)
+Generate_rand_DNA <- function(DNA_len){
+  name_me3 <- sample(c("A", "T", "G", "C"), size = DNA_len, replace = TRUE)
+  name_me4 <- paste0(name_me3, collapse = "")
+  return(name_me4)
+}
